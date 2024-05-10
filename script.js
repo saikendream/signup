3d1dd708435512f6968dd27435208ed4fd42fb6b
Jae <121826115+saikendream@users.noreply.github.com>
2024-05-10 12:45:22 -0300

    PASSWORD validation function created

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let pass = document.querySelector("#u_pass");
 let passConf = document.querySelector("#u_pass-conf");
+
+passConf.addEventListener("input", function(e) {
+    if(passConf.value == pass.value) {
+        passConf.setCustomValidity("Your password doesn't match.");
+    } else {
+        passConf.setCustomValidity("");
+    }
+});
