22fa8009e29516d89e7f4f5c047508ab830c931b
Jae <121826115+saikendream@users.noreply.github.com>
2024-05-14 15:37:27 -0300

    Coded FORM and CLEAR BUTTON EventListeners

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let pass = document.querySelector("#u_pass");
-let passConf = document.querySelector("#u_pass-conf");
+const pass = document.querySelector("#u_pass");
+const passConf = document.querySelector("#u_pass-conf");
+const form = document.querySelector(".main");
+const clear = document.querySelector('button[type="reset"]')
 
 passConf.addEventListener("input", function(e) {
-    if(passConf.value == pass.value) {
+    if(pass.value != passConf.value) {
+        console.log("Password unmatch")
         passConf.setCustomValidity("Your password doesn't match.");
     } else {
+        console.log("Password confirmed.")
         passConf.setCustomValidity("");
     }
+});
+
+form.addEventListener("input", function(e) {
+    let input = e.target.closest("input");
+
+    if(input.value != '') {
+        console.log("Input detedted")
+        input.classList.add("filled");
+    } else {
+        input.classList.remove("filled");
+    }
+});
+
+clear.addEventListener("click", function(e) {
+    console.log("CLEAR clicked");
+    form.querySelectorAll("input").forEach(function(elem) {
+        elem.classList.remove("filled")
+    });
 });
